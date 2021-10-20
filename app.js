c06ca4e6214a82dde09dc2bfb5976aed3c3dab8b
Kevin Do <kevind0501@gmail.com>
2021-10-20 14:36:42 -0700

    updated README, html, and JS

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
 // import functions and grab DOM elements
-
+import pokemon from "./pokemon.js";
 // initialize global state
+
+const poke1Img = document.getElementById('poke-1-img');
+const poke2Img = document.getElementById('poke-2-img');
+const poke3Img = document.getElementById('poke-3-img');
+const button = document.getElementById('submit-button');
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+const encounterPokemon = ()=> {
+    let randPoke1 = Math.floor(Math.random() * pokemon.length);
+    let randPoke2 = Math.floor(Math.random() * pokemon.length);
+    let randPoke3 = Math.floor(Math.random() * pokemon.length);
+
+    while (
+      randPoke1 === randPoke2 || 
+      randPoke1 === randPoke3 ||
+      randPoke2 === randPoke3
+    ) {
+      randPoke1 = Math.floor(Math.random() * pokemon.length);
+      randPoke2 = Math.floor(Math.random() * pokemon.length);
+      randPoke3 = Math.floor(Math.random() * pokemon.length);
+    }
+    console.log(randPoke1, randPoke2)
+
+
+    let poke1 = pokemon[randPoke1];
+    poke1Img.src = poke1.url_image;
+
+    let poke2 = pokemon[randPoke2];
+    poke2Img.src = poke2.url_image;
+
+    let poke3 = pokemon[randPoke3];
+    poke3Img.src = poke3.url_image;
+};
+
+encounterPokemon();
+
+button.addEventListener('click', ()=> {
+    encounterPokemon();
+})
